a407e150fdfaa99c94b570981b14163368b313f1
Muhammed Sefa Yolcu <88800293+sefayolcu6@users.noreply.github.com>
2021-09-09 13:29:06 +0300

    KONSOL ÖRNEĞİ
    C# KONSOL EKRANINDAN KULLANICIDAN BİLGİ İSTEYEN UYGULAMA

---
+++ KONSOL ÖRNEĞİ/c örnekler/Program.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace c_örnekler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //if ile kullanıcı adı şifre ekranı
+            //Console.WriteLine("kullanıcı adı belirleyin");
+            //string kullaniciadi = Console.ReadLine();
+            //Console.WriteLine("şifrenizi belirleyin");
+            //string sifre1 = Console.ReadLine();
+            //Console.WriteLine("belirlediğiniz şifrenizi tekrar girin");
+            //string sifre2 = Console.ReadLine();
+            //if (sifre1 == sifre2)
+            //{
+            //    Console.WriteLine("kullanıcı adı ve şifreniz başarıyla oluşturuldu");
+            //}
+            //else if(sifre2 != sifre1)
+            //{
+            //    Console.WriteLine("hatalı işlem yaptınız");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("hatalı işlem yaptınız");
+            //}
+
+            //hazır string kod(string.compare(karşılaştırma))ile kullanıcı adı şifre ekranı
+            basla:
+            System.Threading.Thread.Sleep(1000);
+            Console.WriteLine("kullanıcı adı belirleyin");
+            string kullaniciadi = Console.ReadLine();
+            if (string.IsNullOrEmpty(kullaniciadi) == true)//(string.IsNullOrEmpty(kullaniciadi) == true) komutu kullanıcı kullaniciadi kısmını boş bırakıp devam edecekse uyarı verir
+            {
+                Console.WriteLine("lütfen kullanıcı adı alanını boş bırakmayın");
+                Console.Clear();
+                goto basla;
+            }
+            else
+            {
+                basla1:
+                Console.WriteLine("şifrenizi belirleyin");
+                string sifre1 = Console.ReadLine();
+                if (kullaniciadi==sifre1)
+                {
+                    Console.WriteLine("kullanıcı adı ve şifre aynı olamaz. Lütfen Tekrar deneyin");
+                    
+                    goto basla;
+                    Console.ReadKey();
+                }
+                else
+                {
+                    if (string.IsNullOrEmpty(sifre1) == true)
+                    {
+                        System.Threading.Thread.Sleep(1000);
+                        Console.WriteLine("lütfen şifre alanını boş bırakmayın");
+                        goto basla1;
+                    }
+                    else
+                    {
+                    basla2:
+                        Console.WriteLine("belirlediğiniz şifrenizi tekrar girin");
+                        string sifre2 = Console.ReadLine();
+                        if (string.IsNullOrEmpty(sifre2) == true)
+                        {
+                            Console.WriteLine("lütfen şifre alanını boş bırakmayın");
+                            goto basla2;
+                        }
+                        else
+                        {
+                            int kontrol = string.Compare(sifre2, sifre1);// int kontrol = string.Compare(sifre2, sifre1) komutu sifre1 ve sifre2 değerlerini karşılaştırır. sonuç aynı ise 0 değerini verir.
+                            if (kontrol == 0)
+                            {
+                                string sifre = sifre2;
+                                Console.WriteLine("kullanıcı adı ve şifreniz başarıyla oluşturuldu. Kullanıcı Adı:{0} Şifre:{1}", kullaniciadi, sifre1);
+                            }
+                            else
+                            {
+                                Console.WriteLine("hatalı işlem yaptınız.");
+                            }
+                            Console.WriteLine("Tekrar denemek ister misiniz? [E,e:Evet,H,h:Hayır]");
+                            char karar = Convert.ToChar(Console.ReadLine());
+                            if (karar == 'E' || karar == 'e')
+                            {
+                                goto basla;
+                            }
+                            else
+                            {
+                                Console.WriteLine("Yine bekleriz birader");
+                            }
+                        }
+                    }
+                }
+
+            }
+
+
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ KONSOL ÖRNEĞİ/c örnekler/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Bir bütünleştirilmiş koda ilişkin Genel Bilgiler aşağıdaki öznitelikler kümesiyle
+// denetlenir. Bütünleştirilmiş kod ile ilişkili bilgileri değiştirmek için
+// bu öznitelik değerlerini değiştirin.
+[assembly: AssemblyTitle("c örnekler")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("c örnekler")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// ComVisible özniteliğinin false olarak ayarlanması bu bütünleştirilmiş koddaki türleri
+// COM bileşenleri için görünmez yapar. Bu bütünleştirilmiş koddaki bir türe
+// erişmeniz gerekirse ComVisible özniteliğini o türde true olarak ayarlayın.
+[assembly: ComVisible(false)]
+
+// Bu proje COM'un kullanımına sunulursa, aşağıdaki GUID tür kitaplığının kimliği içindir
+[assembly: Guid("fbb884b2-67cc-4565-b6c6-6ac4c0de01f8")]
+
+// Bir derlemenin sürüm bilgileri aşağıdaki dört değerden oluşur:
+//
+//      Ana Sürüm
+//      İkincil Sürüm 
+//      Yapı Numarası
+//      Düzeltme
+//
+// Tüm değerleri belirtebilir veya varsayılan Derleme ve Düzeltme Numaralarını kullanmak için
+// aşağıda gösterildiği gibi '*' kullanabilirsiniz:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
